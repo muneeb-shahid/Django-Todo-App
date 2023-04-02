c468bdac0ff9e2a462195aae315fb08a7ff7eaf2
muneeb-shahid <muneebshahid0102@gmail.com>
2023-04-02 12:37:05 +0500

    third

--- todoapp/views.py
+++ todoapp/views.py
@@ -8,7 +8,27 @@ def addToDo(request):
     return HttpResponse("Hello addToDo!")
 
 def calculation(request):
-    a= 12
-    b=8
-    calculation = a+b
-    return HttpResponse("calculation: " + str(a) +"+"+ str(b) + '=' + str(calculation))
+    # a= 12
+    # b=8
+    # calculation = a+b
+    print("Calculator") 
+
+
+    math = int(input("Enter marks of the math subject: "))
+    Urdu = int(input("Enter marks of the Urdu subject: "))
+    isl = int(input("Enter marks of the isl subject: "))
+    eng = int(input("Enter marks of the eng subject: "))
+    chem = int(input("Enter marks of the chem subject: "))
+
+    obtainedMark = math+ Urdu+ isl+eng+chem
+    totalMark = 500
+
+    percentage = (obtainedMark/totalMark)* 100
+    print("The percentage is: ",percentage)
+
+    if(percentage > 90):
+        print("A+ Grade")
+    elif(percentage > 80 and percentage <90  ):
+        print("A Grade")
+    # return HttpResponse("calculation: " + str(a) +"+"+ str(b) + '=' + str(calculation))
+    return HttpResponse("The percentage is: " + str(percentage))
